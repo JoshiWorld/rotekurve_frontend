08c3a0e1fadcf797a5e4ae1d3ed6670adea88b7c
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-21 19:08:10 +0100

    header is now dynamic to actual route

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  header: Map<string, string> = new Map([
+    ['startseite', 'Startseite'],
+    ['eintrittskarte', 'Lang lebe die Eintrittskarte!'],
+    ['swg-fotos', 'SWG-Fotos'],
+    ['ueberuns', 'Über uns'],
+    ['mitgliedschaft', 'Mitgliedschaft'],
+    ['kontakt', 'Kontakt'],
+    ['links', 'Links'],
+  ]);
 
+  constructor(private router: Router) { }
+
+  // Checks routerurl with the header map
+  isActive(route: string): boolean {
+    return this.router.url === '/' + route;
+  }
 }
