b45b5df84de35dfac64b317386bd9436b02eae20
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-22 17:19:18 +0100

    added protected admin routes + login
    Took 29 seconds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,31 @@ import { RouterModule, Routes } from '@angular/router';
 /* COMPONENTS */
 import { StartseiteComponent } from './pages/startseite/startseite.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './pages/admin/login/login.component';
 
 /* ROUTES */
 const routes: Routes = [
+  /* PUBLIC */
   { path: 'startseite', component: StartseiteComponent },
-  { path: '', redirectTo: '/startseite', pathMatch: 'full' },
-  { path: '**', pathMatch: 'full', component: NotFoundComponent },
+  { path: '', redirectTo: '/startseite', pathMatch: 'full' }, // redirect '/' route to '/startseite'
 
+  /* ADMIN - PUBLIC */
+  { path: 'login', component: LoginComponent },
+
+  /* ADMIN - PROTECTED */
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // redirect '/admin' route to '/admin/dashboard'
+    ],
+  },
+
+  /* HANDLING MISMATCHED ROUTES */
+  { path: '**', pathMatch: 'full', component: NotFoundComponent },
 ];
 
 /* MODULE IMPORTS/EXPORTS */
