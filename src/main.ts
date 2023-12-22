1a8493f2437dd4796831692fc1fba01032f2f456
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-22 16:57:50 +0100

    removed alpine

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+// enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+const compilerOptions = {
+  providers: [
+    { provide: 'CompilerOptions', useValue: { interpolation: ['[[', ']]'] }, multi: true },
+  ],
+};
+
+platformBrowserDynamic([{ provide: 'CompilerOptions', useValue: compilerOptions }]).bootstrapModule(AppModule)
   .catch(err => console.error(err));
