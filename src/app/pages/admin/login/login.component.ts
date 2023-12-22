2de396259eb1d04b85412d13229415a19a060af4
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-22 17:36:21 +0100

    user cannot go to /login when already logged in
    Took 3 minutes

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    /* WHEN LOGGED IN, NAVIGATING THE USER TO ADMIN DASHBOARD */
+    if (this.authService.isAuthenticatedUser()) {
+      this.router.navigate(['/admin/dashboard']);
+    }
+  }
 
 }
