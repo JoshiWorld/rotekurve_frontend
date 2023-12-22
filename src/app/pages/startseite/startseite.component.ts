f92508252bf452c156fb7ac1310e6eda9569d629
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-22 16:58:08 +0100

    basic layout with cards for articles + timeline

--- src/app/pages/startseite/startseite.component.ts
+++ src/app/pages/startseite/startseite.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-startseite',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./startseite.component.css']
 })
 export class StartseiteComponent {
+  entered = '0';
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  onMouseEnter(index: number): void {
+    this.entered = index.toString();
+    this.updateStyles();
+  }
+
+  onMouseLeave(): void {
+    // Reset styles when mouse leaves
+    this.entered = '0';
+    this.updateStyles();
+  }
+
+  private updateStyles(): void {
+    // Use Renderer2 to add/remove classes or apply styles dynamically
+    const element = this.el.nativeElement;
+
+    // Clear existing classes
+    ['0', '1', '2'].forEach(i => this.renderer.removeClass(element, `[--i:${i}]`));
+
+    // Add current class
+    this.renderer.addClass(element, `[--i:${this.entered}]`);
+  }
 }
