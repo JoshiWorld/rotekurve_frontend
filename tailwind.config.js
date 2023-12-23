af588896a9054aeed294be2b1eb032f9f3855429
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-23 02:59:17 +0100

    added flowbite
    Took 7 seconds

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -50,6 +51,8 @@ module.exports = {
       ]
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
