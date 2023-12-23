1671aa74f6a30311dda7a32d793969f430143b80
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-23 03:00:12 +0100

    fixed map iterator issue in header
    Took 12 seconds

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,24 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
-  header: Map<string, string> = new Map([
-    ['aktuelles', 'Aktuelles'],
-    ['ueberuns', 'Über uns'],
-    ['spielplan', 'Spielplan'],
-    ['links', 'Links'],
-    ['kontakt', 'Kontakt'],
-  ]);
+export class HeaderComponent implements AfterViewInit {
+  header: Map<string, string>;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService, private cdr: ChangeDetectorRef) {
+    this.header = new Map([
+      ['aktuelles', 'Aktuelles'],
+      ['ueberuns', 'Über uns'],
+      ['spielplan', 'Spielplan'],
+      ['links', 'Links'],
+      ['kontakt', 'Kontakt'],
+    ]);
+  }
+
+  ngAfterViewInit(): void {
+    this.cdr.detectChanges();
+  }
 
   // Checks routerurl with the header map
   isActive(route: string): boolean {
     return this.router.url === '/' + route;
+  }
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticatedUser();
   }
 }
