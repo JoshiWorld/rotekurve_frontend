1be1222528646bb9152df4d9b54530cbd35f26f2
JoshiWorld <joshua.stieber.1903@gmail.com>
2023-12-25 00:20:20 +0100

    added mitglieder component
    Took 10 seconds

---
+++ src/app/pages/admin/mitglieder/mitglieder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mitglieder',
+  templateUrl: './mitglieder.component.html',
+  styleUrls: ['./mitglieder.component.css']
+})
+export class MitgliederComponent {
+
+}
